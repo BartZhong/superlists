b1827671ccac2e0de1cfd0846b51c4fdc869471c
bart <bart@linux-0mek.localdomain>
2016-11-14 22:39:29 +0800

    new URL + view for adding to existing lists. FT

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -38,28 +38,56 @@ class NewVistorTest(LiveServerTestCase):
         #Eds like make fishing by fly
         inputbox.send_keys('Buy peacock feathers')
         
-        #She press enter, page refreshed.
-        #to-do list shows '1: Bug peacock feathers'.
+        #She press enter and be redirected to another URL.
+        #to-do list in the new page shows '1: Bug peacock feathers'.
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(5)
+        time.sleep(1)
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy peacock feathers')  
         #another textbox shown so that she can input other to-dos.
         #she types 'Use peacock feathers to make a fly'
         #Eds do things in orders.
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
-        inputbox.send_keys(Keys.ENTER)
+        inputbox.send_keys(Keys.ENTER)        
         #Home page is refreshed again and there are two to-dos in the list.
-        time.sleep(5)
+        time.sleep(1)
         self.check_for_row_in_list_table('1: Buy peacock feathers')
-        time.sleep(5)
+        time.sleep(1)
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
-        #Eds wants to know if the App can remmenber her list.
-        #she find that the App has generate a unique URL for her.
-        #There are some words to describe the functiona of the App.
+        
+        # now there is a new user access to the site whoes name is Frances
+        ## we use a new browser session
+        ## to ensure edith's data will not leak from cookie
+        self.browser.quit()
+        time.sleep(1)
+        self.browser = webdriver.Firefox(executable_path = '/home/bart/web_dj_sample/TDD_WEB_DJANGO/geckodriver')
+        # Frances access the HomePage
+        # Edith's list is not there
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+        #Frances enter a new to-do and create a new lists
+        #he doesn't feel cool.
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        #Frances get his unique URL
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+        
+        #this page contains no edith's list
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+        
+        #They both feel good and go to sleep then
         self.fail('Finish The Test!')
-        #She access the App again by the URL and her to-do list is still there.
-        #she feels good and go to sleep.
+
 
     
     
